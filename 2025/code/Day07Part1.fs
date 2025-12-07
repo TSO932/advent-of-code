9f09664790953a961833eaf78ae3672b5dc66cf8
Matt L <44550021+TSO932@users.noreply.github.com>
2025-12-07 14:29:57 +0000

    2025 Day 7 Part 1

--- 2025/code/Day07Part1.fs
+++ 2025/code/Day07Part1.fs
@@ -6,18 +6,20 @@ module Day07Part1 =
 
     let splitBeams((count, beamRow), deflectorRow) =
 
-        let defelect ((count, newBe:char[]), (pos, be, ro)) =
+        let deflect((count, newBe:char[]), (pos, be, ro)) =
 
             if ro = '^' && (be = '|' || be = 'S') then
                 newBe.[pos - 1] <- '|'
                 newBe.[pos] <- '.'
                 newBe.[pos + 1] <- '|'
-                (count + 1, newBe) else (count, newBe)
+                (count + 1, newBe)
+            else
+                (count, newBe)
 
         (beamRow, deflectorRow)
         ||> Array.zip
         |> Array.mapi (fun i (b, d) -> (i, b, d))
-        |> Array.fold (fun acc elem -> defelect(acc, elem))  (count, beamRow)
+        |> Array.fold (fun acc elem -> deflect(acc, elem))  (count, beamRow)
 
     let run (input:seq<string>) =
 
