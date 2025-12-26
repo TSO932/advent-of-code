037321d9e1849c40a79434eec0d7e49493cfd2a9
Matt L <44550021+TSO932@users.noreply.github.com>
2025-12-26 18:48:21 +0000

    2025 Day 10 Part 2 WIP

--- 2025/code/Day10Part2.fs
+++ 2025/code/Day10Part2.fs
@@ -60,7 +60,7 @@ module Day10Part2 =
                     |> Array.filter isInRange
 
 
-                printfn "%i %i" idx (newOptions.Length)
+                // printfn "%i %i" idx (newOptions.Length)
 
                 findSwitches(newOptions, buttons, idx + 1)
 
@@ -69,3 +69,60 @@ module Day10Part2 =
     let run(input:seq<string>) =
         input
         |> Seq.sumBy parseLine
+
+    let getParities(input:string) =
+        
+        input.ToCharArray()
+        |> Array.map (fun c -> (c = '1' || c = '3' || c = '5' || c = '7' || c = '9'))
+
+    let parseLine2(input:string) =
+        
+        let parts =
+            input.Split ' '
+
+        let embracedJoltages =
+            parts[parts.Length - 1]
+        
+        let joltages  =
+            embracedJoltages[1 .. embracedJoltages.Length - 2].Split ','
+            |> Array.map (Int32.Parse >> (fun n -> n % 2 = 1))
+
+        printfn "%A" joltages
+
+        let buttonMaker(buts:int[]) =
+
+            buts
+            |> Array.fold (fun (arr:bool[]) n ->
+                
+                 arr[n] <- true
+                 arr
+            ) (Array.zeroCreate<bool> joltages.Length)
+
+        let buttons =
+            parts[1 .. parts.Length - 2]
+            |> Array.map ((fun s -> s[1 .. s.Length - 2]) >> ((fun s -> s.Split ',') >> Array.map Int32.Parse))
+            //|> Array.map buttonMaker
+
+        printfn "%A" buttons
+        
+
+        let rec buttonOptions(butOpts:(int[]*bool)[][], buttons:int[][]) =
+
+            if buttons.Length = 0 then
+                butOpts
+            else
+
+                let nextButton = Array.head buttons
+                let buttonOff = [| (nextButton, false) |]
+                let buttonOn = [| (nextButton, true) |]
+
+                let newButOpts =
+                    butOpts
+                    |> Array.map (fun butOpt -> [| Array.append butOpt buttonOff; Array.append butOpt buttonOn |])
+                    |> Array.concat
+                    
+                buttonOptions(newButOpts, Array.tail buttons)
+
+        // buttonOptions([| ([|-8; -90|], false); ([|-1; -66|], true)  |], buttons)
+        buttonOptions([|[||]|], buttons)
+        //|> Array.length
--- 2025/test/Day10Part2.fs
+++ 2025/test/Day10Part2.fs
@@ -38,3 +38,15 @@ type Day10Part2 () =
             ]
 
         Assert.That(Day10Part2.run(input), Is.EqualTo(33))
+
+
+    [<Test>]
+    member _.GetParity() =
+        Assert.That(Day10Part2.getParities("3547"), Is.EqualTo([|true; true; false; true|]))
+
+    [<Test>]
+    member _.parseLine2_1() =
+    
+        let input = "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}"
+
+        Assert.That(Day10Part2.parseLine2(input), Is.EqualTo(10))
