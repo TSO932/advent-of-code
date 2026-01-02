057eef718c64c7f163fc459716bb6d715aed9e31
Matt L <44550021+TSO932@users.noreply.github.com>
2026-01-02 09:09:32 +0000

    2025 Day 10 Part 2 WIP

--- 2025/code/Day10Part2.fs
+++ 2025/code/Day10Part2.fs
@@ -6,123 +6,70 @@ module Day10Part2 =
 
     let parseLine(input:string) =
         
-        let parts =
-            input.Split ' '
-
-        let embracedJoltages =
-            parts[parts.Length - 1]
+        let parts = input.Split ' '
         
-        let joltages  =
-            embracedJoltages[1 .. embracedJoltages.Length - 2].Split ','
+        let currentJoltages =
+            let input = parts[parts.Length - 1]
+
+            input[1 .. input.Length - 2].Split ','
             |> Array.map Int32.Parse
 
+        let rec doProcess(currentJoltages) = 
 
-        let buttonMaker(buts:int[]) =
-
-            buts
-            |> Array.fold (fun (arr:bool[]) n ->
-                
-                 arr[n] <- true
-                 arr
-            ) (Array.zeroCreate<bool> joltages.Length)
-
-        let buttons =
-            parts[1 .. parts.Length - 2]
-            |> Array.map ((fun s -> s[1 .. s.Length - 2]) >> ((fun s -> s.Split ',') >> Array.map Int32.Parse))
-            |> Array.map buttonMaker
-
-
-        let isInRange(joltageOption:int[]) =
-
-            Array.zip joltageOption joltages
-            |> Array.map (fun (o, j) -> o > j)
-            |> Array.exists id
-            |> not
-
-        let rec findSwitches(joltageOptions:int[][], buttons:bool[][], idx:int) =
-        
-            if Array.contains joltages joltageOptions then
-                idx
+            if Array.sum currentJoltages = 0 then
+                0
             else
-                let newOptions =
-                    joltageOptions
-                    |> Array.map (fun joltageOption ->
-                        
-                        buttons
-                        |> Array.map (fun button ->
+                let currentParities =
+                    currentJoltages
+                    |> Array.map (fun n -> n % 2 = 1)
+
+                let reducedJoltages =
+                    currentJoltages
+                    |> Array.map (fun n -> n / 2)
+
+                let findJoltage(switches:bool[]) =
+
+                    let buttonMaker(buts:int[]) =
+
+                        buts
+                        |> Array.fold (fun (arr:bool[]) n ->
                             
-                            Array.zip joltageOption button
-                            |> Array.map (fun (j, b) -> j + (if b then 1 else 0))
-                        )
-                    )
-                    |> Array.concat
-                    |> Array.distinct
-                    |> Array.filter isInRange
+                            arr[n] <- true
+                            arr
+                        ) (Array.zeroCreate<bool> switches.Length)
 
+                    let buttons =
+                        parts[1 .. parts.Length - 2]
+                        |> Array.map ((fun s -> s[1 .. s.Length - 2]) >> ((fun s -> s.Split ',') >> Array.map Int32.Parse))
+                        |> Array.map buttonMaker
 
-                // printfn "%i %i" idx (newOptions.Length)
+                    let buttonCombinator(combos, button) =
+                        combos
+                        |> Array.map (fun combo -> [| combo; Array.append combo [|button|] |])
+                        |> Array.concat
 
-                findSwitches(newOptions, buttons, idx + 1)
+                    let xor(a, b) =
+                        Array.zip a b
+                        |> Array.map (fun (s, b) -> (s || b) && not (s && b))
 
-        findSwitches([|(Array.zeroCreate<int> joltages.Length)|], buttons, 0)
+                    let foldXor(combos) =
+                        combos
+                        |> Array.fold (fun acc combo -> xor (acc, combo)) (Array.zeroCreate<bool> switches.Length)
+
+                    let buttonCombinations =
+                        buttons
+                        |> Array.fold (fun combos buttons ->  buttonCombinator( combos, buttons )) [| [|  |] |]
+                        |> Array.map (fun combo -> (foldXor combo, Array.length combo))
+                        |> Array.filter (fun ( combo, _ ) -> combo = switches)
+                        |> Array.minBy snd
+                        |> snd
+
+                    buttonCombinations
+
+                findJoltage currentParities
+
+        doProcess currentJoltages
     
     let run(input:seq<string>) =
         input
         |> Seq.sumBy parseLine
-
-    let getParities(input:string) =
-        
-        input.ToCharArray()
-        |> Array.map (fun c -> (c = '1' || c = '3' || c = '5' || c = '7' || c = '9'))
-
-    let parseLine2(input:string) =
-        
-        let parts =
-            input.Split ' '
-
-        let embracedJoltages =
-            parts[parts.Length - 1]
-        
-        let joltages  =
-            embracedJoltages[1 .. embracedJoltages.Length - 2].Split ','
-            |> Array.map (Int32.Parse >> (fun n -> n % 2 = 1))
-
-        printfn "%A" joltages
-
-        let buttonMaker(buts:int[]) =
-
-            buts
-            |> Array.fold (fun (arr:bool[]) n ->
-                
-                 arr[n] <- true
-                 arr
-            ) (Array.zeroCreate<bool> joltages.Length)
-
-        let buttons =
-            parts[1 .. parts.Length - 2]
-            |> Array.map ((fun s -> s[1 .. s.Length - 2]) >> ((fun s -> s.Split ',') >> Array.map Int32.Parse))
-            //|> Array.map buttonMaker
-
-        printfn "%A" buttons
-        
-
-        let rec buttonOptions(butOpts:(int[]*bool)[][], buttons:int[][]) =
-
-            if buttons.Length = 0 then
-                butOpts
-            else
-
-                let nextButton = Array.head buttons
-                let buttonOff = [| (nextButton, false) |]
-                let buttonOn = [| (nextButton, true) |]
-
-                let newButOpts =
-                    butOpts
-                    |> Array.map (fun butOpt -> [| Array.append butOpt buttonOff; Array.append butOpt buttonOn |])
-                    |> Array.concat
-                    
-                buttonOptions(newButOpts, Array.tail buttons)
-
-        // buttonOptions([| ([|-8; -90|], false); ([|-1; -66|], true)  |], buttons)
-        buttonOptions([|[||]|], buttons)
-        //|> Array.length
--- 2025/test/Day10Part2.fs
+++ 2025/test/Day10Part2.fs
@@ -14,21 +14,21 @@ type Day10Part2 () =
         Assert.That(Day10Part2.parseLine(input), Is.EqualTo(10))
 
        
-    [<Test>]
+    // [<Test>]
     member _.parseLine2() =
     
         let input = "[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}"
 
         Assert.That(Day10Part2.parseLine(input), Is.EqualTo(12))
 
-    [<Test>]
+    // [<Test>]
     member _.parseLine3() =
     
         let input = "[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"
 
         Assert.That(Day10Part2.parseLine(input), Is.EqualTo(11))
 
-    [<Test>]
+    // [<Test>]
     member _.Example() =
     
         let input = [
@@ -40,13 +40,14 @@ type Day10Part2 () =
         Assert.That(Day10Part2.run(input), Is.EqualTo(33))
 
 
-    [<Test>]
-    member _.GetParity() =
-        Assert.That(Day10Part2.getParities("3547"), Is.EqualTo([|true; true; false; true|]))
+    // [<Test>]
+    // member _.GetParities() =
 
-    [<Test>]
-    member _.parseLine2_1() =
-    
-        let input = "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}"
+    //     let expected = [|
+    //         ([|true; true; false; true|], 1); // 3 5 4 7
+    //         ([|true; false; false; true|], 2); // 1 2 2 3
+    //         ([|false; true; true; true|], 4) // 0 1 1 1
+    //     |]
+        
+    //     Assert.That(Day10Part2.getParities("{3,5,4,7}"), Is.EqualTo(expected))
 
-        Assert.That(Day10Part2.parseLine2(input), Is.EqualTo(10))
