9179b498ac15c9cbbd1a35678592a18b42754993
Matt L <44550021+TSO932@users.noreply.github.com>
2026-01-02 12:42:44 +0000

    2025 Day 10 Part 2 WIP

--- 2025/code/Day10Part2.fs
+++ 2025/code/Day10Part2.fs
@@ -6,47 +6,43 @@ module Day10Part2 =
 
     let parseLine(input:string) =
         
+        printfn "%s" input
+
         let parts = input.Split ' '
-        
+
         let currentJoltages =
             let input = parts[parts.Length - 1]
 
             input[1 .. input.Length - 2].Split ','
             |> Array.map Int32.Parse
 
-        let rec doProcess(currentJoltages) = 
+        let buttonMaker(buts:int[]) =
+
+            buts
+            |> Array.fold (fun (arr:bool[]) n ->
+                
+                arr[n] <- true
+                arr
+            ) (Array.zeroCreate<bool> currentJoltages.Length)
+
+        let buttonCombinator(combos, button) =
+            combos
+            |> Array.map (fun combo -> [| combo; Array.append combo [|button|] |])
+            |> Array.concat
+
+        let buttonCombos =
+            parts[1 .. parts.Length - 2]
+            |> Array.map ((fun s -> s[1 .. s.Length - 2]) >> ((fun s -> s.Split ',') >> Array.map Int32.Parse))
+            |> Array.map buttonMaker        
+            |> Array.fold (fun combos buttons ->  buttonCombinator( combos, buttons )) [| [|  |] |]
+
+
+        let rec doProcess(currentJoltages) =
 
             if Array.sum currentJoltages = 0 then
                 0
             else
-                let currentParities =
-                    currentJoltages
-                    |> Array.map (fun n -> n % 2 = 1)
-
-                let reducedJoltages =
-                    currentJoltages
-                    |> Array.map (fun n -> n / 2)
-
                 let findJoltage(switches:bool[]) =
-
-                    let buttonMaker(buts:int[]) =
-
-                        buts
-                        |> Array.fold (fun (arr:bool[]) n ->
-                            
-                            arr[n] <- true
-                            arr
-                        ) (Array.zeroCreate<bool> switches.Length)
-
-                    let buttons =
-                        parts[1 .. parts.Length - 2]
-                        |> Array.map ((fun s -> s[1 .. s.Length - 2]) >> ((fun s -> s.Split ',') >> Array.map Int32.Parse))
-                        |> Array.map buttonMaker
-
-                    let buttonCombinator(combos, button) =
-                        combos
-                        |> Array.map (fun combo -> [| combo; Array.append combo [|button|] |])
-                        |> Array.concat
 
                     let xor(a, b) =
                         Array.zip a b
@@ -56,17 +52,30 @@ module Day10Part2 =
                         combos
                         |> Array.fold (fun acc combo -> xor (acc, combo)) (Array.zeroCreate<bool> switches.Length)
 
-                    let buttonCombinations =
-                        buttons
-                        |> Array.fold (fun combos buttons ->  buttonCombinator( combos, buttons )) [| [|  |] |]
-                        |> Array.map (fun combo -> (foldXor combo, Array.length combo))
-                        |> Array.filter (fun ( combo, _ ) -> combo = switches)
-                        |> Array.minBy snd
-                        |> snd
+                    let foldRemainingJoltages(combos) =
 
-                    buttonCombinations
+                        let reduceJoltages(acc, combo) =
+                            (acc, combo)
+                            ||> Array.zip
+                            |> Array.map (fun (a, c) -> max 0 (a - if c then 1 else 0))
 
-                findJoltage currentParities
+                        combos
+                        |> Array.fold (fun acc combo -> reduceJoltages(acc, combo)) currentJoltages
+                        |> Array.map (fun n -> n/2)
+
+                    let clouseau =
+                        buttonCombos
+                        |> Array.map (fun combo -> (foldXor combo, foldRemainingJoltages combo, Array.length combo))
+                        |> Array.filter (fun ( combo, _, _ ) -> combo = switches)|> Array.map (fun (_, remJolts, n) -> n + 2 * doProcess(remJolts))
+
+                    if Array.length clouseau = 0 then
+                        2
+                    else
+                        Array.min clouseau
+
+                currentJoltages
+                |> Array.map (fun n -> n % 2 = 1)
+                |> findJoltage
 
         doProcess currentJoltages
     
