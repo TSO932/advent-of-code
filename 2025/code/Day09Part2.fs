639da6e8b48aefd0dbc791cb9f872f0eb142b23a
Matt L <44550021+TSO932@users.noreply.github.com>
2026-01-08 11:36:00 +0000

    2025 Day 9 Part 2 WIP

--- 2025/code/Day09Part2.fs
+++ 2025/code/Day09Part2.fs
@@ -1,6 +1,7 @@
 namespace _2025
 
 open System
+open System.Collections.Generic
 
 module Day09Part2 =
 
@@ -50,7 +51,11 @@ module Day09Part2 =
             |> List.fold (fun (edges, (x1, y1)) (x2, y2) -> (List.append edges (getEdge (x1, y1) (x2, y2)), (x2, y2))) init
             |> fst
             |> List.distinct
-            |> Set.ofList
+
+        // removes cells which are in the middle of a horizonal line
+        let polygonWithBitsGone =
+            polygon
+            |> List.filter (fun (x, y) -> not ((List.contains (x - 1, y) polygon) && (List.contains (x + 1, y) polygon)))
 
         let getPairs(lst) =
             lst |> List.mapi (fun i a -> lst |> List.mapi (fun j b -> if i < j then [a; b] else []))
@@ -58,7 +63,12 @@ module Day09Part2 =
                 |> List.filter (fun a -> List.length a = 2)
                 |> List.map (fun a -> a[0], a[1])
 
-        let getSquare (x1, y1) (x2, y2) =
+        let getFilledSquare square =
+
+            let x1 = square |> List.map fst |> List.min
+            let x2 = square |> List.map fst |> List.max
+            let y1 = square |> List.map snd |> List.min
+            let y2 = square |> List.map snd |> List.max
 
             let vert =
                 if y2 > y1 then
@@ -75,30 +85,74 @@ module Day09Part2 =
             |> List.concat
 
 
+        let getSquare (x1, y1) (x2, y2) =
+            
+            let vert =
+                if y2 > y1 then
+                    List.init (y2 - y1 + 1) (fun v -> y1 + v)
+                        else
+                    List.init (y1 - y2 + 1) (fun v -> y2 + v)           
+            let horiz =
+                if x2 > x1 then
+                    List.init (x2 - x1 + 1) (fun v -> x1 + v)
+                else
+                    List.init (x1 - x2 + 1) (fun v -> x2 + v)
+
+            let top = horiz |> List.map (fun x -> (x, y1))
+            let bottom = horiz |> List.map (fun x -> (x, y2))
+            let left = vert |> List.map (fun y -> (x1, y))
+            let right = vert |> List.map (fun y -> (x2, y))
+            
+            top
+            |> List.append bottom
+            |> List.append left
+            |> List.append right
+            |> List.distinct
+
         let maxX = Seq.length compressedX
+        let cache = Dictionary<_,_>()
 
-// memorise this one?
         let pointInPolygon x y =
+            match cache.TryGetValue((x, y)) with
+            | true, memorisedAnswer -> memorisedAnswer
+            | _ -> 
 
-            if Set.contains (x, y) polygon then
-                printfn "YES %i %i" x y
-                true
-            else
-                printfn "NOT %i %i" x y
-                let rayCast =
-                    List.init (maxX - x) (fun v -> (x + v, y))
-                    |> Set.ofList
+                if List.contains (x, y) polygon then
+                    true
+                else
+                    let inPolygon =
+                        polygonWithBitsGone
+                        |> List.filter (fun (_, y1) -> y1 = y)
+                        |> List.map fst
+                        |> List.filter (fun x1 -> x1 > x)
+                        |> List.length
+                        |> fun n -> n % 2 = 1    
 
-                (polygon, rayCast)
-                ||> Set.intersect
-                |> Set.count
-                |> fun n -> n % 2 = 1            
+                    cache.Add((x,y), inPolygon)
+                    inPolygon       
 
         let squareInPolygon square =
-            square
-            |> List.map (fun (x, y) -> pointInPolygon x y)
-            |> List.contains false
-            |> not
+
+            let sample =
+                List.randomChoices ((List.length square) / 10) square
+
+            if sample
+                |> List.map (fun (x, y) -> pointInPolygon x y)
+                |> List.contains false then false
+            else
+                square
+                |> List.map (fun (x, y) -> pointInPolygon x y)
+                |> List.contains false
+                |> not
+
+        let cornersInPolygon (x1, y1) (x2, y2) =
+                pointInPolygon x1 y1
+                &&
+                pointInPolygon x2 y2
+                &&
+                pointInPolygon x1 y2
+                &&
+                pointInPolygon x2 y1
 
         let convertBack square =
             let x1 = square |> List.map fst |> List.min
@@ -108,8 +162,15 @@ module Day09Part2 =
             ((revX[x1], revY[y1]), (revX[x2], revY[y2]))
 
         getPairs compressedXY
+        |> List.filter (fun (a, b) -> cornersInPolygon a b)
         |> List.map (fun (a, b) -> getSquare a b)
         |> List.filter squareInPolygon
+        // |> List.map getFilledSquare
+        // |> List.filter squareInPolygon
         |> List.map convertBack
         |> List.map (fun ((x1, y1), (x2, y2)) -> ((1L + Math.Abs(x2 - x1)) * (1L + Math.Abs(y2 - y1))))
         |> List.max
+
+
+// 140762692 too low
+// 140762692 same answer with filled square included
